d54255b787d49aebe77159cf2cdf17522db4f983
chunkingz <fortuneking33@yahoo.com>
2021-09-24 12:49:04 +0100

    chore: create blog frontend
    - add blog home page
    - add individual dynamic blog pages
    - add tagging functionality
    - add request/feedback form
    - add footer
    - add read more button to individual blog posts
    - add tags to each blog post on home page
    - add social links
    - add social share links to each blog post
    - make blog responsive
    - add form validation on submit
    - add sweetalert on form submit
    - trigger email alert on for contact form submit
    - add success page
    - add navbar to match main website nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FormSuccessComponent } from './components/form-success/form-success.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlogComponent},
+  { path: 'blog-post/:year/:month/:day/:url', component: BlogPostComponent},
+  { path: 'contact-us', component: ContactFormComponent},
+  { path: 'form-success', component: FormSuccessComponent},
+  // { path: '**', component: BlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,60 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SummaryPipe } from './pipes/summary.pipe';
+import { BlogPostComponent } from './components/blog-post/blog-post.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FormSuccessComponent } from './components/form-success/form-success.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ShareModule } from 'ngx-sharebuttons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ShareIconsModule } from 'ngx-sharebuttons/icons';
+import { ShareButtonModule } from 'ngx-sharebuttons/button';
+
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BlogComponent,
+    FooterComponent,
+    SideBarComponent,
+    SummaryPipe,
+    BlogPostComponent,
+    ContactFormComponent,
+    FormSuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ShareModule,
+    MarkdownModule.forRoot(),
+    FontAwesomeModule,
+    ShareIconsModule,
+    ShareButtonModule,
+    ShareButtonsModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+     library.addIconPacks(fas, fab);
+   } 
+}
