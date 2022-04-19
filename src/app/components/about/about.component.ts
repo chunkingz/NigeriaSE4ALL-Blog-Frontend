be319b12dc0f63a1c7a9481fb3c40900e00ff918
AlexanderWeiss-Motz <aweiss-motz@integration.org>
2022-04-19 15:40:49 +0200

    first round of re-desgin buttons, charts, anout page, hero video

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { homeAnimation } from '../animation/animation';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [ homeAnimation ]
 })
 export class AboutComponent implements OnInit {
 
